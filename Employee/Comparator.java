6faaddc2d8dfe36b458f096703ffc940ac1f23c7
UmmayAbiha <83369767+UmmayAbiha@users.noreply.github.com>
2022-02-22 01:51:10 +0530

    Delete Employee directory

--- Employee/Comparator.java
+++
@@ -1,6 +0,0 @@
-package com.example.Employee;
-
-public interface Comparator<T> {
-    public int compare(T e1, T e2);
-
-}
--- Employee/SalarySort.java
+++
@@ -1,9 +0,0 @@
-package com.example.Employee;
-
-import java.util.Comparator;
-
-class SalarySort implements Comparator<Employee> {
-    public int compare(Employee e1, Employee e2){
-        return e1.Salary.compareTo(e2.Salary);
-    }
-}
